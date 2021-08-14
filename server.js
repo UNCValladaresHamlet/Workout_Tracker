7ec84b0908f34d279e4b9a3f4a3827aae7fd7999
Hamlet Valladares <sigorahv@gmail.com>
2021-08-14 11:57:01 -0400

    deploying

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  { 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false});
+  useFindAndModify: false
+  }
+);
 
   app.use(require("./routes/html.js"));
   app.use(require("./routes/api.js"));
