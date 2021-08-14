036c80d1b39d6321cc32d7e2b2a82ba6d969e9e0
Hamlet Valladares <sigorahv@gmail.com>
2021-08-14 12:14:05 -0400

    changed workout.js model

--- models/workout.js
+++ models/workout.js
@@ -1,28 +1,40 @@
+
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  type: {
-    type: String,
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date()
   },
-  name: {
-    type: String
-  },
-  duration: {
-    type: Number
-  },
-  weight: {
-    type: Number
-  },
-  reps:{
-    type: Number
-  },
-  sets:{
-    type: Number
-  }
+  exercises: [
+      {
+        type: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+          duration: {
+            type: Number,
+          },
+          distance: {
+            type: Number,
+          },
+          weight: {
+            type: Number,
+          },
+          reps: {
+            type: Number,
+          },
+          sets: {
+            type: Number,
+          },
+      }
+  ],
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
